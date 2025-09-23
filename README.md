831dc2fadd97dfcc07f784083417656971f5d44e
Evan Hock <hocke@oregonstate.edu>
2025-09-23 05:31:22 -0700

    Add documentation, modify manifest.json to support gecko ID, add jsdoc config and modify .gitignore to reflect.

--- README.md
+++ README.md
@@ -6,7 +6,6 @@ Browser extension that tweaks the [Lingodeer Website](https://lingodeer.com/) by
 that I felt were missing. Currently just modifies the exercises so that the textareas have smaller placeholder text,
 automatically focus when they appear, and you can press tab when the textarea is on screen to focus on it.
 
-
 ## Future Plans
 
 If I feel like tweaking this further, I would like to
--- content_script.js
+++ content_script.js
@@ -1,12 +1,30 @@
 "use strict"
 
+/** The placeholder text that textareas should now have. */
 const PLACEHOLDER = "Enter text."
 
+/** Determines whether or not a node is a LingoDeer course element.
+ * Used to check whether or not we are doing a lesson.
+ * @param {*} node
+ * @returns {boolean}
+ */
 function isMainCourseElement(node) {
     return node instanceof Element && node.classList.contains("mainCourse")
 }
 
+/**
+ * Class that provides the textarea improvement features, namely:
+ * 
+ * * During non-dialogue lessons, textareas are automatically focused.
+ * * The placeholder text for textareas is now shorter.
+ * * The Tab key automatically focus textareas while they are on screen.
+ */
 class TextFocusFeatureProvider {
+    /**
+     * Sets up the provision of the QoL improvements this class provides.
+     * @param {string} placeholder - The string to replace the placeholder text of textareas with
+     * @param {Element} mainCourse - The course element to inject into
+     */
     constructor(placeholder, mainCourse) {
         this.placeholder = placeholder
 
@@ -26,20 +44,40 @@ class TextFocusFeatureProvider {
         })
     }
 
+    /**
+     * Determines whether or not the lesson is a dialogue lesson.
+     * @param {Element} mainCourse - The mainCourse element of the lesson
+     * @returns {boolean}
+     */
     static isDialogue(mainCourse) {
         return mainCourse.classList.contains("dialogMode")
     }
 
+    /**
+     * Injects the functionality of this class in the provided course element.
+     * @param {Element} mainCourse
+     * @returns {TextFocusFeatureProvider}
+     */
     static inject(mainCourse) {
         return TextFocusFeatureProvider.isDialogue(mainCourse) ? null : new TextFocusFeatureProvider(PLACEHOLDER, mainCourse)
     }
 
+    /**
+     * Modifies the placeholder text of the provided textarea,
+     * focuses it, and sets up the event listener for the Tab
+     * shortcut focus.
+     * @param {HTMLTextAreaElement} textarea
+     */
     patchTextArea(textarea) {
         textarea.placeholder = this.placeholder
         textarea.focus()
         this.registerTabFocus(textarea)
     }
 
+    /**
+     * Sets up the event listener for the Tab focus feature.
+     * @param {HTMLTextAreaElement} textarea 
+     */
     registerTabFocus(textarea) {
         this.tabFocusEventListener?.unregister()
 
@@ -57,6 +95,7 @@ class TextFocusFeatureProvider {
         document.body.addEventListener("keydown", this.tabFocusEventListener)
     }
 
+    /** Frees all resources used by this class. */
     cleanup() {
         this.textareaObserver.disconnect()
         this.tabFocusEventListener?.unregister()
